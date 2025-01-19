4126cb1465d12b1e651884e96fd8f88230a8f7f6
Adrian <adekgoik58@gmail.com>
2025-01-19 19:44:51 +0100

    lab07

--- Lab07/Autoencoder.py
+++ Lab07/Autoencoder.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
 import os
+import matplotlib
+matplotlib.use('Agg')  # Użycie backendu 'Agg' dla zapisu obrazów
 
 # Ścieżka do katalogu z danymi
 DATA_DIR = "C:/Users/Adrian/Desktop/KNO_repo/Lab07/photos"
@@ -10,7 +12,7 @@ DATA_DIR = "C:/Users/Adrian/Desktop/KNO_repo/Lab07/photos"
 IMG_SIZE = (128, 128)
 BATCH_SIZE = 32
 
-# Wczytanie danych z katalogu
+# wczytywanie danych
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
     DATA_DIR,
     image_size=IMG_SIZE,
@@ -29,8 +31,8 @@ augmentation = tf.keras.Sequential([
 
 dataset = dataset.map(lambda x: (augmentation(x), x))
 
-# Definicja autoenkodera
-latent_dim = 48
+# przestrzeń latentna - length of my sequences https://blog.keras.io/building-autoencoders-in-keras.html
+latent_dim = 2
 
 # Encoder
 encoder = models.Sequential([
@@ -41,15 +43,17 @@ encoder = models.Sequential([
     layers.Dense(latent_dim)
 ])
 
-# Decoder
+# Decoder - dodałem więcej warstw konwolucyjnych próbująć uzyskać lepszy obrazek
 decoder = models.Sequential([
     layers.Input(shape=(latent_dim,)),
-    layers.Dense(32 * 32 * 64, activation='relu'),
-    layers.Reshape((32, 32, 64)),
+    layers.Dense(16 * 16 * 128, activation='relu'),
+    layers.Reshape((16, 16, 128)),
+    layers.Conv2DTranspose(128, (3, 3), activation='relu', strides=2, padding='same'),
     layers.Conv2DTranspose(64, (3, 3), activation='relu', strides=2, padding='same'),
     layers.Conv2DTranspose(32, (3, 3), activation='relu', strides=2, padding='same'),
     layers.Conv2DTranspose(3, (3, 3), activation='sigmoid', padding='same')
 ])
+
 
 # Autoencoder
 inputs = layers.Input(shape=(128, 128, 3))
@@ -61,7 +65,19 @@ autoencoder = models.Model(inputs, decoded)
 autoencoder.compile(optimizer='adam', loss='mse')
 
 # Trening
-autoencoder.fit(dataset, epochs=20)
+autoencoder.fit(dataset, epochs=50)
+
+# Generowanie obrazka z latent space (autoencoder bez transfer learning)
+latent_vector = np.array([[0.5, -0.5]])  # Losowe wartości w przestrzeni latentnej
+generated_image = decoder.predict(latent_vector)
+
+# Zapis obrazka wygenerowanego przez autoenkoder
+import matplotlib.pyplot as plt
+plt.imshow(generated_image[0])
+plt.axis('off')
+plt.title("Generated Image - Autoencoder")
+plt.savefig("generated_image_autoencoder.png")  # Zapis obrazka
+plt.show()
 
 # Zadanie dodatkowe: Transfer Learning
 base_model = tf.keras.applications.MobileNetV2(input_shape=(128, 128, 3), include_top=False, weights='imagenet')
@@ -82,16 +98,15 @@ transfer_autoencoder = models.Model(transfer_inputs, transfer_decoded)
 transfer_autoencoder.compile(optimizer='adam', loss='mse')
 
 # Trening autoenkodera z transfer learning
-transfer_autoencoder.fit(dataset, epochs=20)
+transfer_autoencoder.fit(dataset, epochs=50)
 
-# Generowanie obrazka z latent space
-latent_vector = np.random.uniform(-1, 1, (1, 48))  # Losowe wartości w przestrzeni latentnej
+# Generowanie obrazka z latent space (autoencoder z transfer learning)
+transfer_latent_vector = np.array([[0.5, -0.5]])  # Losowe wartości w przestrzeni latentnej
+transfer_generated_image = decoder.predict(transfer_latent_vector)
 
-  # Współrzędne w przestrzeni latentnej
-generated_image = decoder.predict(latent_vector)
-
-# Zapis wygenerowanego obrazka
-import matplotlib.pyplot as plt
-plt.imshow(generated_image[0])
+# Zapis obrazka wygenerowanego przez autoenkoder z transfer learning
+plt.imshow(transfer_generated_image[0])
 plt.axis('off')
-plt.savefig("generated_image.png")
+plt.title("Generated Image - Transfer Learning")
+plt.savefig("generated_image_transfer_learning.png")
+plt.show()
